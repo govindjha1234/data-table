7f904f3db4fc552c0947429c8d2bd4e4f47ff014
govind <govindjha7654@gmail.com>
2022-01-25 14:19:53 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,183 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, Fragment } from "react";
+import "./App.css";
+import data from "./mock-data.json";
+import { nanoid } from "nanoid";
+import ReadOnlyRow from "./components/ReadOnlyRow";
+import EditableRow from "./components/EditableRow";
+
 
 function App() {
+  const [contacts, setContacts] = useState(data);
+  const [addFormData, setAddFormData] = useState({
+    fullName: "",
+    address: "",
+    phoneNumber: "",
+    email: "",
+  });
+  const [editFormData, setEditFormData] = useState({
+    fullName: "",
+    address: "",
+    phoneNumber: "",
+    email: "",
+  });
+
+  const [editContactId, setEditContactId] = useState(null);
+
+  const handleAddFormChange = (event) => {
+    event.preventDefault();
+
+    const fieldName = event.target.getAttribute("name");
+    const fieldValue = event.target.value;
+
+    const newFormData = { ...addFormData };
+    newFormData[fieldName] = fieldValue;
+
+    setAddFormData(newFormData);
+  };
+  const handleEditFormChange = (event) => {
+  event.preventDefault();
+
+  const fieldName = event.target.getAttribute("name");
+  const fieldValue = event.target.value;
+
+  const newFormData = {...editFormData};
+  newFormData[fieldName] = fieldValue;
+
+  setEditFormData(newFormData);
+
+  };
+
+  const handleAddFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newContact = {
+      id: nanoid(),
+      fullName: addFormData.fullName,
+      address: addFormData.address,
+      phoneNumber: addFormData.phoneNumber,
+      email: addFormData.email,
+    };
+    const newContacts = [...contacts, newContact];
+    setContacts(newContacts);
+  };
+
+  const handleEditFormSubmit = (event) => {
+ event.preventDefault();
+
+ const editedContact = {
+   id: editContactId,
+  fullName: editFormData.fullName,
+  address: editFormData.address,
+  phoneNumber:editFormData.phoneNumber,
+  email: editFormData.email,
+ }
+ const newContacts = [...contacts];
+
+ const index = contacts.findIndex((contact)=> contact.id === editContactId );
+ newContacts[index] = editedContact;
+
+ setContacts(newContacts);
+ setEditContactId(null)
+  };
+  const handleEditClick = (event, contact) => {
+    event.preventDefault();
+    setEditContactId(contact.id);
+
+    const formValues = {
+      fullName: contact.fullName,
+      address: contact.address,
+      phoneNumber: contact.phoneNumber,
+      email: contact.email,
+    };
+    setEditFormData(formValues);
+  };
+  const handleCancelClick = () => {
+    setEditContactId(null);
+  };
+  const handleDeleteClick = (contactId) => {
+    const newContacts = [...contacts];
+
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+
+    newContacts.splice(index, 1);
+
+    setContacts(newContacts);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-container">
+      
+      <h1 className="heading"><u>USER DATA TABLE</u></h1>
+      <form onSubmit={handleEditFormSubmit}>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Address</th>
+              <th>Phone number</th>
+              <th>Email</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {contacts.map((contact) => (
+              
+              <Fragment>
+                 {editContactId === contact.id ? (
+                  <EditableRow  editFormData={editFormData}
+                   handleEditFormChange={handleEditFormChange}
+                   handleCancelClick={handleCancelClick}
+                   
+                   />
+                ) : (
+                  <ReadOnlyRow
+                    contact={contact}
+                    handleEditClick={handleEditClick}
+                    handleDeleteClick={handleDeleteClick}
+                  />
+                  
+                )}
+                
+              </Fragment>
+            ))}
+          </tbody>
+        </table>
+      </form>
+
+      <h2 className="head"><span>Add a contact </span></h2>
+      <form onSubmit={handleAddFormSubmit}>
+        <input
+          type="text"
+          name="fullName"
+          required="required"
+          placeholder="Enter a name.."
+          onChange={handleAddFormChange}
+        />
+        <input
+          type="text"
+          name="address"
+          required="required"
+          placeholder="Enter an address.."
+          onChange={handleAddFormChange}
+        />
+        <input
+          type="text"
+          name="phoneNumber"
+          required="required"
+          placeholder="Enter a phone number.."
+          onChange={handleAddFormChange}
+        />
+        <input
+          type="email"
+          name="email"
+          required="required"
+          placeholder="Enter an email..."
+          onChange={handleAddFormChange}
+        />
+        <button type="submit">Add</button>
+      </form>
+      <br />
+     <small className="sm" > contact us : 7761013343</small>
     </div>
   );
 }
---
+++ src/components/EditableRow.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+function EditableRow({editFormData, handleEditFormChange, handleCancelClick}) {
+  return (
+      <tr>
+          <td>
+              <input
+              type="text"
+              required="required"
+              placeholder='Enter a name'
+              name='fullName'
+              value={editFormData.fullName}
+              onChange={handleEditFormChange}
+              ></input>
+          </td>
+          <td>
+          <input
+              type="text"
+              required="required"
+              placeholder='Enter an address'
+              name='address'
+              value={editFormData.address}
+              onChange={handleEditFormChange}
+              ></input>
+          </td>
+          <td>
+          <input
+              type="text"
+              required="required"
+              placeholder='Enter a phone number'
+              name='phoneNumber'
+              value={editFormData.PhoneNumber}
+              onChange={handleEditFormChange}
+              ></input>
+          </td>
+          <td>
+          <input
+              type="email"
+              required="required"
+              placeholder='Enter an email'
+              name='email'
+              value={editFormData.email}
+              onChange={handleEditFormChange}
+              ></input>
+          </td>
+          <td>
+              <button type='submit'>save</button>
+              <button type='button' onClick={handleCancelClick}>
+                  Cancel
+                  </button>
+          </td>
+          
+      </tr>
+  )
+
+}
+
+export default EditableRow;
